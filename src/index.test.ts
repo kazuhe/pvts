73e43ac9d7ea16f2da6e1e1b9e6af81a4742ce12
kazuhe <omc.ohara@gmail.com>
2023-06-22 01:21:03 +0900

    string() と required() を実装

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,65 @@
 import { describe, test, expect } from "vitest";
-import { hello } from "@/index";
+import { isString, isRequired, pvts } from "@/index";
 
-describe("hello", () => {
-  test("returns 'Hello World!'", () => {
-    expect(hello()).toBe("Hello World!");
+describe("isString", () => {
+  const ERROR_MESSAGE = "Error Message";
+  const string = isString(ERROR_MESSAGE);
+
+  test.each([
+    ["", ""],
+    ["0", "0"],
+    ["false", "false"],
+    ["undefined", "undefined"],
+    ["null", "null"],
+  ])("引数が '%s' のとき、'%s' を返していること", (value, expected) => {
+    expect(string(value)).toBe(expected);
+  });
+
+  test.each([[1], [true], [undefined], [null]])(
+    "引数が '%s' のとき、エラーを throw していること",
+    (value) => {
+      expect(() => string(value)).toThrowError(ERROR_MESSAGE);
+    }
+  );
+});
+
+describe("isRequired", () => {
+  const ERROR_MESSAGE = "Error Message";
+  const required = isRequired(ERROR_MESSAGE);
+
+  test.each([
+    [-1, -1],
+    [0, 0],
+    [1, 1],
+    [true, true],
+    [false, false],
+    ["0", "0"],
+    ["false", "false"],
+    ["undefined", "undefined"],
+    ["null", "null"],
+  ])("引数が '%s' のとき、'%s' を返していること", (value, expected) => {
+    expect(required(value)).toBe(expected);
+  });
+
+  test.each([[undefined], [null], [""]])(
+    "引数が '%s' のとき、エラーを throw していること",
+    (value) => {
+      expect(() => required(value)).toThrowError(ERROR_MESSAGE);
+    }
+  );
+});
+
+describe("pvts", () => {
+  test("string() と required() のチェーン", () => {
+    const validator = pvts()
+      .string("文字列を入力してください。")
+      .required("値を入力してください。")
+      .test();
+
+    expect(validator("foo")).toBe(true);
+    expect(validator("")).toBe(false);
+    expect(validator(9)).toBe(false);
+    expect(validator(null)).toBe(false);
+    expect(validator()).toBe(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,58 @@
-const message = "Hello World!";
+type Primitive = string | number | boolean | undefined | null;
 
-export const hello = () => {
-  return message;
+type Validator = (value: Primitive) => Primitive;
+
+/**
+ * 文字列が入力されているか検証する
+ */
+export const isString =
+  (message: string): Validator =>
+  (value) => {
+    if (typeof value !== "string") {
+      throw new Error(message);
+    }
+    return value;
+  };
+
+/**
+ * 何らかの値が入力されているか検証する
+ */
+export const isRequired =
+  (message: string): Validator =>
+  (value) => {
+    if (value === undefined || value === null || value === "") {
+      throw new Error(message);
+    }
+    return value;
+  };
+
+export const pvts = () => {
+  const operations: Array<Validator> = [];
+
+  const string = (message: string) => {
+    operations.push(isString(message));
+    return validators;
+  };
+
+  const required = (message: string) => {
+    operations.push(isRequired(message));
+    return validators;
+  };
+
+  const validators = {
+    string,
+    required,
+    test: () => (value?: Primitive) => {
+      for (const operation of operations) {
+        try {
+          operation(value);
+        } catch (error) {
+          return false;
+        }
+      }
+      return true;
+    },
+  };
+
+  return validators;
 };
